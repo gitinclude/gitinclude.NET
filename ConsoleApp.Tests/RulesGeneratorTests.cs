960ab8eeff3564ab932d12b7e497dd62bd751052
SymboLinker <SymboLinker@gmail.com>
2022-04-04 12:52:39 +0200

    Folder re-strucutre: move unit tests to a folder that is tracked by git

---
+++ ConsoleApp.Tests/RulesGeneratorTests.cs
@@ -0,0 +1,55 @@
+using Xunit;
+
+namespace gitinclude.tests
+{
+    public class RulesGeneratorTests
+    {
+        [Theory]
+        [InlineData(
+@"StrategicProject/SpecificProject1/Method1/doc/*.tex",
+        @"*
+!StrategicProject/
+StrategicProject/*
+!StrategicProject/SpecificProject1/
+StrategicProject/SpecificProject1/*
+!StrategicProject/SpecificProject1/Method1/
+StrategicProject/SpecificProject1/Method1/*
+!StrategicProject/SpecificProject1/Method1/doc/
+StrategicProject/SpecificProject1/Method1/doc/*
+!StrategicProject/SpecificProject1/Method1/doc/*.tex")]
+        [InlineData(
+@"StrategicProject/SpecificProject1/Method1/doc/*.tex
+StrategicProject/SpecificProject1/Method1/doc/*.lyx",
+        @"*
+!StrategicProject/
+StrategicProject/*
+!StrategicProject/SpecificProject1/
+StrategicProject/SpecificProject1/*
+!StrategicProject/SpecificProject1/Method1/
+StrategicProject/SpecificProject1/Method1/*
+!StrategicProject/SpecificProject1/Method1/doc/
+StrategicProject/SpecificProject1/Method1/doc/*
+!StrategicProject/SpecificProject1/Method1/doc/*.lyx
+!StrategicProject/SpecificProject1/Method1/doc/*.tex")]
+        [InlineData(
+@"StrategicProject/SpecificProject1/Method1/doc/*.tex
+StrategicProject/SpecificProject1/Method1/doc/*.lyx
+StrategicProject/readme.md", 
+        @"*
+!StrategicProject/
+StrategicProject/*
+!StrategicProject/readme.md
+!StrategicProject/SpecificProject1/
+StrategicProject/SpecificProject1/*
+!StrategicProject/SpecificProject1/Method1/
+StrategicProject/SpecificProject1/Method1/*
+!StrategicProject/SpecificProject1/Method1/doc/
+StrategicProject/SpecificProject1/Method1/doc/*
+!StrategicProject/SpecificProject1/Method1/doc/*.lyx
+!StrategicProject/SpecificProject1/Method1/doc/*.tex")]
+        public void Test(string include, string ignore)
+        {
+            Assert.Equal(ignore, new RulesGenerator().GetIgnoreRulesFromIncludeRules(include));
+        }
+    }
+}
