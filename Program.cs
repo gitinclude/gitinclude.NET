e7a192bf6f448da02617c728e1a4dd23d5beb463
SymboLinker <SymboLinker@gmail.com>
2022-04-04 11:34:30 +0200

    Add RulesGenerator + unit tests

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+static void Main(string[] args)
+{
+    if (args.Length == 0)
+    {
+        throw new ArgumentException("Provide the path of the folder that contains the .gitinclude file.");
+    }
+    var arg = args[0].Trim().Trim('"').Trim('\'');
+    if (arg.EndsWith(".gitignore"))
+    {
+        arg = arg.Substring(0, arg.Length - ".gitignore".Length);
+    }
+    arg = arg.Trim('\\').Trim('/');
+
+    // get .gitinclude content
+
+    // call RulesGenerator
+
+    // update specific section of .gitignore
+}
---
+++ RulesGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace gitinclude
+{
+    public class RulesGenerator
+    {
+        public string GetIgnoreRulesFromIncludeRules(string includesText)
+        {
+            var includes = Regex.Split(includesText, @"(\r\n|\r|\n)").Select(x => x.Trim()).Where(x => x != "").ToArray();
+
+            var ignoreRules = new List<string>();
+            foreach(var include in includes)
+            {
+                var pathParts = include.Split('/');
+                ignoreRules.Add("!" + pathParts[0] + "/");
+
+                ignoreRules.Add(pathParts[0] + "/*");
+                for (var i = 2; i < pathParts.Length; i++)
+                {
+                    var path = string.Join('/', pathParts.Take(i));
+                    ignoreRules.Add("!" + path + "/");
+                    ignoreRules.Add(path + "/*");
+                }
+                ignoreRules.Add("!" + include);
+            }
+            ignoreRules = ignoreRules.Distinct().OrderBy(x => x.Count(x => x == '/')).ThenBy(x => x.TrimStart('!')).ToList();
+            return "*" + Environment.NewLine + String.Join(Environment.NewLine, ignoreRules);
+        }
+    }
+}
